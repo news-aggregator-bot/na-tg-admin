1133446074491a332b1af52443ec0e416389d62e
v.yemelianov <v.yemelianov@voverc.com>
2021-08-29 23:03:03 +0300

    updated to na-tg-admin

--- scripts/swarm-update.sh
+++ scripts/swarm-update.sh
@@ -1,4 +1,4 @@
 wget -q $1 -O docker-stack-tg-admin.yml
 echo "Deploying new na-tg-admin stack"
 docker stack rm na-tg-admin
-docker stack deploy --compose-file docker-stack-bot.yml --with-registry-auth na-tg-admin
+docker stack deploy --compose-file docker-stack-tg-admin.yml --with-registry-auth na-tg-admin
